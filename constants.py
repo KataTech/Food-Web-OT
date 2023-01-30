7f2f35564351ee3d22a3cc5effd9957d401ea591
KataTech <kmh9@rice.edu>
2023-01-30 00:24:21 -0600

    Deleted unnecessary constants

--- constants.py
+++ constants.py
@@ -1,8 +1,2 @@
 # Directory to Food Web Data
 FOOD_WEB_URL = "data/African_Mammal_FoodWebs.xlsx"
-
-# Directory to Scratch Results
-SCRATCH_PATH = "scratch/results"
-
-# Seaborn Color Map for Visualizations
-SNS_CMAP = "BuPu"
